72e35c54f55dd262eb5035fb1b77a958d2aa32d9
mujarino <157873167+mujarino@users.noreply.github.com>
2025-06-01 05:05:56 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,33 +1,16 @@
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/mujarino/test/refs/heads/main/Rayfield%20Lib%20Source.lua"))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "Test UI",
-    LoadingTitle = "Loading...",
-    LoadingSubtitle = "Rayfield Test",
-    ConfigurationSaving = {
-        Enabled = false
-    },
-    KeySystem = false
+	Name = "Rayfield Example Window",
+	LoadingTitle = "Rayfield Interface Suite",
+	LoadingSubtitle = "by Sirius",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = "Rayfield Interface Suite",
+		FileName = "Big Hub"
+	},
+	KeySystem = false
 })
 
-local MainTab = Window:CreateTab("Main", 4483362458)
-
-local myToggle = false
-
-MainTab:CreateToggle({
-    Name = "Example Toggle",
-    CurrentValue = false,
-    Flag = "ExampleToggle",
-    Callback = function(value)
-        myToggle = value
-        print("Toggle is now:", value)
-    end,
-})
-
-MainTab:CreateButton({
-    Name = "Example Button",
-    Callback = function()
-        print("Button clicked. Toggle is:", myToggle)
-        Rayfield:Notify("Button Pressed", "You clicked the button", 4483362458)
-    end,
-})
+local Tab = Window:CreateTab("Main Tab", 4483362458)
+Tab:CreateLabel("If you see this, Rayfield is working!")
